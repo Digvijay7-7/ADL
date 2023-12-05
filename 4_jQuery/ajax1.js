d36278e1d49694e634cdb5129e9826e295ba6b43
Digvijay Patil <digwijaypatil6521@gmail.com>
2023-12-06 00:05:33 +0530

    new

---
+++ 4_jQuery/ajax1.js
@@ -0,0 +1,8 @@
+console.log("Ajax tutorials");
+
+let fetchBtn = document.getElementById('fetchBtn');
+fetchBtn.addEventListener('click', buttonClickHandler)
+
+function buttonClickHandler() {
+    console.log('You clicked')
+}
